ff8c5ac3712a9f270c6711b4839267e162273cf6
Haggai Yaniv <haggai@getjaco.com>
2015-10-09 18:00:14 +0300

    added some css impoves

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ import angular     from 'angular';
 import uiRouter    from 'angular-ui-router';
 import ngMessages  from 'angular-messages';
 import appStyle    from 'app.less';
-import sjTree   from 'common/directives/tree/tree.js';
+import sjTree   from 'common/directives/tree/tree.js'
 import TabsController   from 'tabs/tabs-controller.js';
 import basicForm   from 'basicForm/basicForm.js';
-import HelpController from 'partials/help/help.js'
-import SignInController from 'views/auth/sign_in.js'
-import AboutController from 'views/about/about.js'
-import CitiesController from 'views/cities/cities.js'
-import BasicHeaderController from 'partials/header/basic_header.js'
-import SignedInHeaderController from 'partials/header/signed_in_header.js'
+import HelpController from 'partials/help/help.js';
+import SignInController from 'views/auth/sign_in.js';
+import AboutController from 'views/about/about.js';
+import CitiesController from 'views/cities/cities.js';
+import BasicHeaderController from 'partials/header/basic_header.js';
+import SignedInHeaderController from 'partials/header/signed_in_header.js';
 import { UserService } from 'common/services/user_service';
 import { registerStates, logErrors, routingRules } from 'routes.js'
 
